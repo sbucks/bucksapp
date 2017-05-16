630651846b0d40486c84dd2b8444e3350bbd97a5
Siva <sbhamidimarri@bahamabucks.com>
2017-05-16 10:22:00 -0500

    angular service node link update

--- angular-src/src/app/services/reveldata.service.ts
+++ angular-src/src/app/services/reveldata.service.ts
@@ -6,21 +6,20 @@ import 'rxjs/add/operator/map';
 export class ReveldataService {
 
   fid: any;
-  date: any;
   constructor(private http: Http) { }
 
   getRevelData(fid) {
     const headers = new Headers();
     headers.append('Content-type', 'application/json');
     console.log('entered the Service ' + JSON.stringify(fid));
-    return this.http.post('entries/values', JSON.stringify(fid), {headers: headers}).map(res => res.json());
+    return this.http.post('http://localhost:3000/entries/values', JSON.stringify(fid), {headers: headers}).map(res => res.json());
   }
 
   getPartyData(week) {
     const headers = new Headers();
     headers.append('Content-type', 'application/json');
     console.log('entered the Service ' + JSON.stringify(week));
-    return this.http.post('entries/partyvalues', JSON.stringify(week), {headers: headers}).map(res => res.json());
+    return this.http.post('http://localhost:3000/entries/partyvalues', JSON.stringify(week), {headers: headers}).map(res => res.json());
   }
 
 }
